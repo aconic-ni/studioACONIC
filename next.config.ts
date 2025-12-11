2675556d8b7e75eea047eba88e5db21f2bc1c44c
Jordy Stvaer <jerco27@gmail.com>
2025-12-11 18:47:35 +0000

    revisa si la pagina inicial esta redireccionaso a /AconicExaminer aun no

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/AconicExaminer',
-  assetPrefix: '/AconicExaminer/',
 
   typescript: {
     ignoreBuildErrors: true,
