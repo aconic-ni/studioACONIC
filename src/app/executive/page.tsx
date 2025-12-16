8497374eaa32979d1e8f0b9bd7bafce029ab57ae
Jordy Stvaer <jerco27@gmail.com>
2025-12-16 00:04:43 +0000

    Try fixing this error: `Runtime TypeError: caseActions.setSelectedCaseFo

--- src/app/executive/page.tsx
+++ src/app/executive/page.tsx
@@ -725,6 +725,7 @@ function ExecutivePageContent() {
     setSelectedCaseForHistory,
     setSelectedIncidentForDetails,
     setSelectedCaseForComment,
+    setSelectedCaseForProcess,
     handleSearchPrevio,
     setCaseToArchive,
     setCaseToDuplicate,
